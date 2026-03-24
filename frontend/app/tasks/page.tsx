64046cd340a40c59e2f7c080a0ea3c4bdc6cc660
vivit123 <vivitphruktayanon@gmail.com>
2026-03-24 23:32:03 +0700

    fetch tasks from laravel api

--- frontend/app/tasks/page.tsx
+++ frontend/app/tasks/page.tsx
@@ -1,7 +1,23 @@
-export default function TasksPage() {
+async function getTasks() {
+  const res = await fetch("http://127.0.0.1:8000/api/tasks", {
+    cache: "no-store"
+  })
+
+  return res.json()
+}
+
+export default async function TasksPage() {
+  const tasks = await getTasks()
+
   return (
     <div>
       <h1>Task List</h1>
+
+      <ul>
+        {tasks.map((task: any) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
